0ae879fa233da089a3d10aeaed81c192e39d0c66
Situ William <situ_william@yahoo.com>
2018-02-13 23:38:00 -0500

    finished adding dynamic states

--- src/MyList.js
+++ src/MyList.js
@@ -5,9 +5,8 @@ class MyList extends Component {
   constructor(props){
     super()
     this.state = {
-      toDoItemArray: props.theList.map((item,index) => {
-        return <ListItem doThis={item} key={index}/>
-      })
+      toDoItemArray: props.theList,
+      newItem: ""
     }
   }
 
@@ -16,16 +15,34 @@ class MyList extends Component {
       toDoItemArray: []
     })
   }
+  newItemChange(e){
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+  addItem(e){
+    e.preventDefault()
+    let toDos = this.state.toDoItemArray
+    toDos.push(this.state.newItem)
+    this.setState({
+      toDoItemArray: toDos,
+      newItem: ""
+    })
+  }
   render() {
-    let todoItems = this.props.theList.map((item,index) => {
+    let todoItems = this.state.toDoItemArray.map((item,index) => {
       return <ListItem doThis={item} key={index}/>
     })
     return (
       <div>
         <h1>Things I should stop procrastinating:</h1>
         <ul>
-          {this.state.toDoItemArray}
+          {todoItems}
         </ul>
+        <form>
+          <input type="text" onChange={(e) => this.newItemChange(e)} value={this.state.newItem}/>
+          <button onClick={(e) => this.addItem(e)}>Add new todo.</button>
+        </form>
         <button onClick={(e) => this.clearList(e)}>Clear this list</button>
       </div>
     );
